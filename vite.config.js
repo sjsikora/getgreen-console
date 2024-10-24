5a3f0760a68459f716f80c53f49fc5f82011f366
Sam <sikora.samj@gmail.com>
2024-10-24 18:37:04 +0000

    Update Config

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: "https://api.emeraldtechnology.net/v1",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  base: "/getgreen-console/"
 })
