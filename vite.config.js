2f899c231fb2b5e48970d94883331e10b7868faf
Sam <sikora.samj@gmail.com>
2024-10-24 19:52:54 +0000

    Misc

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     host: true,
     port: 5173,
-    proxy: {
-      '/api': {
-        target: "https://api.emeraldtechnology.net/v1",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
   }
 })
